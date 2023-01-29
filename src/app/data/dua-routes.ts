37e50f1eee7bb59ab7d20b7e0225655da89984de
BijanProgrammer <bijaneisapour@gmail.com>
2023-01-29 06:50:08 +0330

    [ADD] mafatih 314

--- src/app/data/dua-routes.ts
+++ src/app/data/dua-routes.ts
@@ -4,8 +4,9 @@ import {DuaRouteData} from '../types/dua-route-data.type';
 import {DuaComponent} from '../pages/dua/dua.component';
 
 const pathToTitleMap: Map<string, string> = new Map([
-    ['mafatih312', 'دعای ندبه'],
     ['mafatih39', 'زیارت امام زمان (عج) در روز جمعه'],
+    ['mafatih312', 'دعای ندبه'],
+    ['mafatih314', 'دعای عهد'],
 ]);
 
 const duaRoutes: (Route & {data: DuaRouteData})[] = [...pathToTitleMap.entries()].map(([path, title]) => ({
