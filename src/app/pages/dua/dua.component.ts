623b351da50311da7fbcd691dd953e07cf223ea3
BijanProgrammer <bijaneisapour@gmail.com>
2024-01-30 08:09:08 +0330

    [ADD] go to previous/next faraaz keyboard shortcuts

--- src/app/pages/dua/dua.component.ts
+++ src/app/pages/dua/dua.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {DuaService} from './dua.service';
 import {ActivatedRoute} from '@angular/router';
 import {Subscription} from 'rxjs';
@@ -10,12 +10,20 @@ import {DuaRouteData} from '../../types/dua-route-data.type';
     styleUrls: ['./dua.component.scss'],
 })
 export class DuaComponent implements OnInit, OnDestroy {
+    @ViewChild('dua') public duaElementRef!: ElementRef<HTMLDivElement>;
+
     public highlightedFaraazIndex: number | null = null;
     public highlightedTokenIndex: number | null = null;
 
     private activatedRouteDataSubscription?: Subscription;
 
     public constructor(public duaService: DuaService, public activatedRoute: ActivatedRoute) {}
+
+    private get faraazes(): HTMLDivElement[] {
+        const duaElement = this.duaElementRef.nativeElement;
+        const faraazNodeList = duaElement.querySelectorAll<HTMLDivElement>('.faraaz');
+        return Array.from(faraazNodeList);
+    }
 
     public ngOnInit(): void {
         this.activatedRouteDataSubscription = this.activatedRoute.data.subscribe(async (data) => {
@@ -27,6 +35,18 @@ export class DuaComponent implements OnInit, OnDestroy {
         this.activatedRouteDataSubscription?.unsubscribe();
     }
 
+    @HostListener('window:keydown.PageUp', ['$event'])
+    public pageUpKeyHandler(event: KeyboardEvent): void {
+        event.preventDefault();
+        this.goToPreviousFaraaz();
+    }
+
+    @HostListener('window:keydown.PageDown', ['$event'])
+    public pageDownKeyHandler(event: KeyboardEvent): void {
+        event.preventDefault();
+        this.goToNextFaraaz();
+    }
+
     public tokenMouseEnterHandler(faraazIndex: number, tokenIndex: number): void {
         this.highlightedFaraazIndex = faraazIndex;
         this.highlightedTokenIndex = tokenIndex;
@@ -35,5 +55,45 @@ export class DuaComponent implements OnInit, OnDestroy {
     public tokenMouseLeaveHandler(): void {
         this.highlightedFaraazIndex = null;
         this.highlightedTokenIndex = null;
+    }
+
+    private goToPreviousFaraaz(): void {
+        const nearestIndex = this.findNearestFaraazIndex();
+
+        if (window.scrollY > this.faraazes[nearestIndex].offsetTop) {
+            window.scrollTo({top: this.faraazes[nearestIndex].offsetTop});
+        } else if (nearestIndex - 1 >= 0) {
+            window.scrollTo({top: this.faraazes[nearestIndex - 1].offsetTop});
+        } else {
+            window.scrollTo({top: 0});
+        }
+    }
+
+    private goToNextFaraaz(): void {
+        const nearestIndex = this.findNearestFaraazIndex();
+
+        if (window.scrollY < this.faraazes[nearestIndex].offsetTop) {
+            window.scrollTo({top: this.faraazes[nearestIndex].offsetTop});
+        } else if (nearestIndex + 1 < this.faraazes.length) {
+            window.scrollTo({top: this.faraazes[nearestIndex + 1].offsetTop});
+        } else {
+            window.scrollTo({top: document.body.scrollHeight});
+        }
+    }
+
+    private findNearestFaraazIndex(): number {
+        let nearestElementIndex = 0;
+        let minimumDistance = Number.MAX_VALUE;
+
+        for (let i = 0; i < this.faraazes.length; i++) {
+            const distance = Math.abs(this.faraazes[i].offsetTop - window.scrollY);
+
+            if (minimumDistance > distance) {
+                minimumDistance = distance;
+                nearestElementIndex = i;
+            }
+        }
+
+        return nearestElementIndex;
     }
 }
