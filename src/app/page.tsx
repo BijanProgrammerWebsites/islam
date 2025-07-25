00e7a16676c3a98b92a907b48bc7b4433939ba6f
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-25 15:38:28 +0330

    [ADD] ignore folder to .gitignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { ReactNode } from "react";
+
 import styles from "./page.module.css";
 
-export default function Page() {
+export default function Page(): ReactNode {
   return <div className={styles.page}>Hello, friend!</div>;
 }
